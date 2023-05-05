29e935afe2ff2a28a0dd58d36cb35bca3bc7e241
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-05-05 16:20:06 -0300

    test: adicionados testes para o componente ProjectCard

--- src/components/shared/__tests__/ProjectCard.spec.js
+++ src/components/shared/__tests__/ProjectCard.spec.js
@@ -3,18 +3,26 @@ import "@testing-library/jest-dom";
 import { AppProviders } from "providers/AppProviders";
 import ProjectCard from "components/shared/ProjectCard";
 import { projectsList } from "utils/projects";
+import { faker } from "@faker-js/faker";
 
 describe("ProjectCard", () => {
+    const title = faker.name.jobTitle();
+    const description = faker.lorem.paragraph();
+    const url = faker.internet.url();
+    const thumb = faker.internet.url();
+
+    const windowOpenSpy = jest.spyOn(window, 'open').mockImplementation(() => {});
+
     const renderProjectCard = () => {
         render(
             <AppProviders>
                 <ProjectCard
-                    title="Título do projeto"
-                    description="Descrição do projeto"
+                    title={title}
+                    description={description}
                     highlights={projectsList[0].highlights}
-                    thumbUrl="https://github.com/YannFigueiredo/assets/blob/main/yannfigueiredo.github.io/thumbs/productions-guide.png?raw=true"
-                    githubUrl="https://github.com/"
-                    projectUrl="https://www.google.com/"
+                    thumbUrl={url}
+                    githubUrl={url}
+                    projectUrl={thumb}
                 />
             </AppProviders>
         );
@@ -23,14 +31,14 @@ describe("ProjectCard", () => {
     it("Tela principal precisa renderizar corretamente", () => {
         renderProjectCard();
 
-        const thumb = screen.getByAltText("Título do projeto");
-        const titleCard = screen.getByText("Título do projeto");
-        const descriptionCard = screen.getByText("Descrição do projeto");
+        const thumbCard = screen.getByAltText(title);
+        const titleCard = screen.getByText(title);
+        const descriptionCard = screen.getByText(description);
         const infoBtn = screen.getByTestId("btn-info");
         const githubBtn = screen.getByTestId("btn-github");
         const linkBtn = screen.getByTestId("btn-link");
         
-        expect(thumb).toBeInTheDocument();
+        expect(thumbCard).toBeInTheDocument();
         expect(titleCard).toBeInTheDocument();
         expect(descriptionCard).toBeInTheDocument();
         expect(infoBtn).toBeInTheDocument();
@@ -43,6 +51,7 @@ describe("ProjectCard", () => {
 
         const secondaryScreen = screen.getByTestId("project-card-secondary-screen");
         const closeBtn = screen.getByTestId("btn-close");
+        const highlight = screen.getByText("O projeto contém paginação, filtros, pesquisa e carrosel de itens.");
         
         expect(secondaryScreen).toBeInTheDocument();
         expect(closeBtn).toBeInTheDocument();
@@ -53,6 +62,7 @@ describe("ProjectCard", () => {
             left: 0,
             right: 0
         });
+        expect(highlight).toBeInTheDocument();
     });
 
     it("Tela secundária precisa ser deslocada verticalmente para 0% quando o botão de informação da tela principal for clicado", () => {
@@ -81,5 +91,23 @@ describe("ProjectCard", () => {
         expect(secondaryScreen).toHaveStyle({
             transform: "translateY(-100%)"
         });
+    });
+
+    it("O botão de github precisa renderizar com href e target corretos para abrir a url em outra aba do navegador", () => {
+        renderProjectCard();
+
+        const githubBtn = screen.getByTestId("btn-github");
+
+        expect(githubBtn).toHaveAttribute('href', url);
+        expect(githubBtn).toHaveAttribute('target', '_blank');
+    });
+
+    it("O botão de link do projeto precisa renderizar com href e target corretos para abrir a url em outra aba do navegador", () => {
+        renderProjectCard();
+
+        const infoBtn = screen.getByTestId("btn-github");
+
+        expect(infoBtn).toHaveAttribute('href', url);
+        expect(infoBtn).toHaveAttribute('target', '_blank');
     });
 });
