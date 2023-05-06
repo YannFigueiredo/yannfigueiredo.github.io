63f88ecc329095a03262b4fe8a85665d33860ea1
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-05-06 10:50:01 -0300

    refactor: adicionados styled components e classes para o componente JobCard

--- src/components/shared/JobCard/index.js
+++ src/components/shared/JobCard/index.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { Container, Header, Main } from "./styles";
+import { Container, Header, Main, ContentWrapper, SkillsCarrousel } from "./styles";
 
 export default function JobCard({
     imageCompanyUrl,
@@ -32,24 +32,29 @@ export default function JobCard({
                 </p>
             </Header>
             <Main ref={mainContent} data-testid="job-main">
-                <h3>{companyName} - {jobName}</h3>
-                <p>
-                    {description}
-                </p>
-                <span>Tecnologias:</span>
-                <div>
-                    <div>
-                        {skillsUrlList.map((skill, key) =>
-                            <div key={key}>
-                                <img 
-                                    src={skill.url}
-                                    alt={skill.name}
-                                    title={skill.name}
-                                />
-                            </div>
-                        )}
+                <ContentWrapper>
+                    <h3>{companyName}</h3>
+                    <span>{jobName}</span>
+                    <p>
+                        {description}
+                    </p>
+                </ContentWrapper>
+                <SkillsCarrousel>
+                    <span>Tecnologias:</span>
+                    <div className="skills-wrapper">
+                        <div className="skills">
+                            {skillsUrlList.map((skill, key) =>
+                                <div className="skill" key={key}>
+                                    <img 
+                                        src={skill.url}
+                                        alt={skill.name}
+                                        title={skill.name}
+                                    />
+                                </div>
+                            )}
+                        </div>
                     </div>
-                </div>
+                </SkillsCarrousel>
             </Main>
         </Container>
     );
--- src/components/shared/JobCard/styles.js
+++ src/components/shared/JobCard/styles.js
@@ -7,3 +7,7 @@ export const Container = styled.article`
 export const Header = styled.header``
 
 export const Main = styled.div``
+
+export const contentWrapper = styled.div``
+
+export const SkillsCarrousel = styled.div``
