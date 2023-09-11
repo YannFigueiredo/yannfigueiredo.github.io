1c2e9fcb8011bf7cbca00f8cf01836b0fe90d06f
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-09-11 09:19:40 -0300

    fix: corrigida imagem do logo da lacrei saúde

--- src/utils/jobs.js
+++ src/utils/jobs.js
@@ -1,6 +1,8 @@
+import lacreiSaudeLogo from "assets/images/company_lacrei_saude.jpg";
+
 export const jobsList = [
     {
-        imageCompanyUrl: "https://media.licdn.com/dms/image/D4D0BAQHQfd4dN0LqWg/company-logo_200_200/0/1680525177533?e=1691625600&v=beta&t=PdE2lNY8_aNaIWXyKIVTKff84sBl1YxxmLyLzeQvNWI",
+        imageCompanyUrl: lacreiSaudeLogo,
         companyName: "Lacrei Saúde",
         jobName: "Desenvolvedor Front-End",
         description: "Contribuí no desenvolvimento de uma plataforma inclusiva que vai possibilitar a conexão com profissionais da área da saúde para eventuais pacientes da comunidade LGBTQIA+. No projeto, onde é utilizada a metodologia SCRUM, criei páginas e componentes flexíveis e reutilizáveis a partir de modelos no figma, além disso integrei a API da plataforma no front-end.",
