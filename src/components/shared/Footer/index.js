cde982d8eec7694bd28c7e9767091270aa69bea3
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-01-29 20:10:52 -0300

    refactor: alterado texto do footer

--- src/components/shared/Footer/index.js
+++ src/components/shared/Footer/index.js
@@ -3,7 +3,7 @@ import { Container } from "./styles";
 export default function Footer() {
     return(
         <Container data-testid="footer">
-            <span>&copy; Yann Fabricio Cardoso de Figueiredo, 2022</span>
+            <span>&copy; Yann Fabricio Cardoso de Figueiredo | 2022</span>
             <span>yannfabricio@gmail.com</span>
         </Container>
     );
