2ca21e1364ab1c859eac798eb22a276fe23321ef
Yann Figueiredo <yann.fabricio@hotmail.com>
2024-04-22 16:19:09 -0300

    refactor: ajustado o texto da seção 'sobre'

--- src/components/sections/About/index.js
+++ src/components/sections/About/index.js
@@ -20,22 +20,7 @@ export default function About() {
                     de dados, documentação de software, testes e inteligência artificial.
                 </p>
                 <p>
-                    Possuo experiência prática em um projeto real de voluntariado na ONG Lacrei Saúde, onde 
-                    participei do desenvolvimento de uma plataforma inclusiva para conectar profissionais da 
-                    saúde com pacientes da comunidade LGBTQIA+. Nesse projeto, pude aplicar na prática o 
-                    desenvolvimento ágil com SCRUM e aprender e aprimorar tecnologias como Next.js, JavaScript, 
-                    TypeScript, consumo de API com Axios, versionamento de código com git, React Hook Form e 
-                    construção de componentes responsivos e fiéis a modelos do figma. É um projeto 
-                    muito importante onde desenvolvi muito minhas habilidades técnicas, comunicação, trabalho 
-                    em equipe e otimização de tempo de codificação.
-                </p>
-                <p>
-                    Fazendo projetos pessoais foquei em utilizar o framework React para desenvolver projetos 
-                    cada vez mais otimizados e completos, como o meu mais recente, Productions Guide, onde 
-                    trabalhei diversas competências como consumo de API com axios, testes com jest, estilização 
-                    e componentização com styled components, e gerenciamento de estados com context api. 
-                    Atualmente, além de me aprimorar no front-end, estou praticando back-end com Node.js/Express.js 
-                    em novos projetos pessoais.
+                    Sou um Desenvolvedor Front-End com 6 meses de experiência no mercado, atuando pela ONG Lacrei Saúde. Participei do desenvolvimento de uma plataforma inclusiva que visa conectar profissionais da saúde com pacientes da comunidade LGBTQIA+, com o intuito de fornecer opções de atendimento médico mais humanizado. Nesse projeto apliquei na prática o desenvolvimento ágil com SCRUM e aprendi e/ou aprimorei tecnologias como React, Next, JavaSript, TypeScript, Styled Comppnents, consumo de API com Axios, versionamento de código com Git e construção de componentes responsivos, reutilizáveis e fiéis a modelos do figma elaborados pela equipe de UX/UI Design. É um projeto muito importante, onde desenvolvi muito minhas habilidades técnicas, comunicação, trabalho em equipe, capacidade para resolução de problemas, otimização de SEO, código limpo (clean code) e melhores práticas de programação. Atualmente, além de me aprimorar no front-end, estou praticando back-end com Java e Spring Boot.
                 </p>
             </TextWrapper>
         </Container>
