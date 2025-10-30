532f49d734d82036cf56a7708a5df5411977ccf4
Yann Figueiredo <yann.figueiredo@dotkon.co>
2025-10-30 20:30:35 -0300

    feat: adiciona experiência profissional na dotkon

--- src/utils/jobs.js
+++ src/utils/jobs.js
@@ -1,47 +1,73 @@
 import lacreiSaudeLogo from "assets/images/companies-logos/company_lacrei_saude.jpg";
+import dotkonLogo from "assets/images/companies-logos/company_dotkon.jpg";
 
 export const jobsList = [
-    {
-        imageCompanyUrl: lacreiSaudeLogo,
-        companyName: "Lacrei Saúde",
-        jobName: "Desenvolvedor Front-End",
-        description: "Contribuí no desenvolvimento de uma plataforma inclusiva que vai possibilitar a conexão com profissionais da área da saúde para eventuais pacientes da comunidade LGBTQIA+. No projeto, onde é utilizada a metodologia SCRUM, criei páginas e componentes flexíveis e reutilizáveis a partir de modelos no figma, além disso integrei a API da plataforma no front-end.",
-        initialData: "01/2023",
-        finalData: "06/2023",
-        actualJob: false,
-        skillsUrlList: [
-            {
-                name: "React.js",
-                url: "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
-            },
-            {
-                name: "Next.js",
-                url: "https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white"
-            },
-            {
-                name: "Gatsby.js",
-                url: "https://img.shields.io/badge/Gatsby-663399?style=for-the-badge&logo=gatsby&logoColor=white"
-            },
-            {
-                name: "HTML",
-                url: "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
-            },
-            {
-                name: "CSS",
-                url: "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
-            },
-            {
-                name: "JavaScript",
-                url: "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E"
-            },
-            {
-                name: "TypeScript",
-                url: "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
-            },
-            {
-                name: "Styled Components",
-                url: "https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white"
-            }
-        ]
-    }
+  {
+    imageCompanyUrl: lacreiSaudeLogo,
+    companyName: "Lacrei Saúde",
+    jobName: "Desenvolvedor Front-End",
+    description:
+      "Contribuí para o desenvolvimento de uma plataforma inclusiva que conecta profissionais da saúde com pacientes da comunidade LGBTQIAPN+. Desenvolvi interfaces responsivas e componentes reutilizáveis com React, Next.js e Styled Components, seguindo protótipos do Figma. Também atuei na integração de APIs REST no front-end, com foco em acessibilidade, performance e boas práticas de código.",
+    initialData: "01/2023",
+    finalData: "06/2023",
+    actualJob: false,
+    skillsUrlList: [
+      {
+        name: "React.js",
+        url: "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      },
+      {
+        name: "Next.js",
+        url: "https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white",
+      },
+      {
+        name: "JavaScript",
+        url: "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E",
+      },
+      {
+        name: "TypeScript",
+        url: "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "Styled Components",
+        url: "https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white",
+      },
+    ],
+  },
+  {
+    imageCompanyUrl: dotkonLogo,
+    companyName: "Dotkon",
+    jobName: "Desenvolvedor Front-End",
+    description:
+      "Atuo no desenvolvimento de soluções web para diversos segmentos, como farmacêutico, agronegócios, construção civil e transporte. Trabalho com React, TypeScript, Chakra UI e Ant Design (AntD) na criação de interfaces modernas e performáticas. Também realizo consumo de APIs REST (Axios) e integrações GraphQL, além de atuar com testes automatizados (Playwright) e desenvolvimento colaborativo em times ágeis (Scrum).",
+    initialData: "06/2024",
+    finalData: null,
+    actualJob: true,
+    skillsUrlList: [
+      {
+        name: "React.js",
+        url: "https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB",
+      },
+      {
+        name: "TypeScript",
+        url: "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+      },
+      {
+        name: "Chakra UI",
+        url: "https://img.shields.io/badge/Chakra--UI-319795?style=for-the-badge&logo=chakra-ui&logoColor=white",
+      },
+      {
+        name: "Ant Design",
+        url: "https://img.shields.io/badge/Ant%20Design-1890FF?style=for-the-badge&logo=antdesign&logoColor=white",
+      },
+      {
+        name: "Playwright",
+        url: "https://img.shields.io/badge/Playwright-45ba4b?style=for-the-badge&logo=Playwright&logoColor=white",
+      },
+      {
+        name: "Storybook",
+        url: "https://img.shields.io/badge/storybook-FF4785?style=for-the-badge&logo=storybook&logoColor=white",
+      },
+    ],
+  },
 ];
